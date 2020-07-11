01e09dbd4f5b06b859485e9fb7ebe5906b3db269
pasanprem <46164432+pasanprem@users.noreply.github.com>
2020-07-11 12:14:28 +0100

    Add project files.

---
+++ AnimalClasses/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClasses
+{
+    class Animal
+    {
+
+        Boolean isAlive { get; set; }
+        int weight { get; set; }
+
+        public Animal()
+        {
+            Console.WriteLine("Animal Constructor");
+        }
+        public Animal(Boolean isAlive, int w)
+        {
+            this.isAlive = isAlive;
+            this.weight = w;
+
+            Console.WriteLine("Animal Constructor");
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Animal says 'hello'");
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine("Animal is talking");
+        }
+
+        public void Sing()
+        {
+            Console.WriteLine("Animal is singing");
+        }
+
+        override public string ToString()
+        {
+            return "Status: " + isAlive + " Weight: " + weight;
+        }
+    }
+}
---
+++ AnimalClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Animal beast = new Animal(true, 100);
+
+            beast.Greet();
+            beast.Talk();
+            beast.Sing();
+
+            Console.WriteLine(beast.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
